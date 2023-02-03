46ca3212cfc3864e05e518a31d0b4740f8378c45
afazad <abulfajle405@gmail.com>
2023-02-03 22:14:16 +0530

    Initial Commit

---
+++ Permutation.java
@@ -0,0 +1,17 @@
+public class Permutation {
+    public static void printPermutation(String str, String permutation){
+        if(str.length() ==0){
+            System.out.println(permutation);
+            return;
+        }
+        for(int i=0; i<str.length(); i++){
+            char  newChar = str.charAt(i);
+            String  newStr = str.substring(0, i)+str.substring(i+1);
+            printPermutation(newStr, permutation+newChar); 
+        }
+    }
+    public static void main(String args[]){
+        String  str = "xyz";
+        printPermutation(str, ""); 
+    }
+}
